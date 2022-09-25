78c90e6a1dd0e09b2952e45128732e1f08897aa2
tomasotano25 <tomasotano25@gmail.com>
2022-09-24 22:17:42 -0400

    Clase 02 nuevo proyecto

---
+++ src/ClassState.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class ClassState extends React.Component {
+  constructor() {}
+
+  render() {
+    return (
+      <div>
+        <h2>Eliminar ClassState</h2>
+        <p>
+          Por favor, escribe el codigo de seguridad para comprobar que quieres
+          eliminar
+        </p>
+
+        <input placeholder="Codigo de Seguridad" />
+        <button>Comprobar</button>
+      </div>
+    );
+  }
+}
+
+export { ClassState };
---
+++ src/UseState.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function UseState() {
+  return (
+    <div>
+      <h2>Eliminar UseState</h2>
+      <p>
+        Por favor, escribe el codigo de seguridad para comprobar que quieres
+        eliminar
+      </p>
+
+      <input placeholder="Codigo de Seguridad" />
+      <button>Comprobar</button>
+    </div>
+  );
+}
+
+export { UseState };
