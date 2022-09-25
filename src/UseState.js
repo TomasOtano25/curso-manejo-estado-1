42c824d5979c62ab72a7ba9fd8ed24d3ebd0ace3
tomasotano25 <tomasotano25@gmail.com>
2022-09-25 10:37:47 -0400

    Clase 09 estados compuestos con useState

--- src/UseState.js
+++ src/UseState.js
@@ -3,23 +3,42 @@ import React from "react";
 const SECURITY_CODE = "paradigma";
 
 function UseState({ name }) {
-  const [value, setValue] = React.useState("");
-  const [error, setError] = React.useState(false);
-  const [loading, setLoading] = React.useState(false);
+  const [state, setState] = React.useState({
+    value: "",
+    error: false,
+    loading: false,
+  });
+
+  console.log(state);
+
+  //Estados simples
+  // const [value, setValue] = React.useState("");
+  // const [error, setError] = React.useState(false);
+  // const [loading, setLoading] = React.useState(false);
 
   React.useEffect(() => {
     console.log("Empezando el efecto");
 
-    if (!!loading) {
+    if (!!state.loading) {
       setTimeout(() => {
         console.log("Haciendo la validacion");
 
-        if (value === SECURITY_CODE) {
-          setLoading(false);
+        if (state.value === SECURITY_CODE) {
+          setState({
+            ...state,
+            loading: false,
+            error: false,
+          });
+          // setLoading(false);
           // setError(false);
         } else {
-          setLoading(false);
-          setError(true);
+          setState({
+            ...state,
+            loading: false,
+            error: true,
+          });
+          // setLoading(false);
+          // setError(true);
         }
 
         console.log("Terminando la validacion");
@@ -27,7 +46,7 @@ function UseState({ name }) {
     }
 
     console.log("Terminando el efecto");
-  }, [loading]);
+  }, [state.loading]);
 
   return (
     <div className="m-12">
@@ -39,16 +58,17 @@ function UseState({ name }) {
         </p>
       </div>
 
-      {error && !loading && (
+      {state.error && !state.loading && (
         <p className="text-red-400">Error: El codigo es incorrecto</p>
       )}
-      {loading && <p className="text-green-400">Cargando...</p>}
+      {state.loading && <p className="text-green-400">Cargando...</p>}
 
       <input
-        value={value}
+        value={state.value}
         onChange={(event) => {
           // setError(false); // Este fue
-          setValue(event.target.value);
+          // setValue(event.target.value);
+          setState({ ...state, value: event.target.value });
         }}
         type="text"
         className="border border-gray-200 rounded-md px-1 py-1 mr-2 outline-none focus:ring-2 focus:ring-blue-400"
@@ -58,7 +78,8 @@ function UseState({ name }) {
         className="py-1 px-2 rounded-md text-white font-bold bg-blue-400 hover:bg-blue-500"
         onClick={() => {
           // setError(false); // Este fue
-          setLoading(true);
+          // setLoading(true);
+          setState({ ...state, loading: true });
         }}
       >
         Comprobar
